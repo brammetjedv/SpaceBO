b65cb17c9daa7feb40ea9866fb77f0ed6bb7547c
Damon Jellema <damon.jellema2004@gmail.com>
2021-05-12 12:40:15 +0200

    EnemySpawner

---
+++ Broken Space/Assets/GenerateEnemies.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 30)
+        {
+            xPos = Random.Range(-20, 20);
+            zPos = Random.Range(-20, 20);
+            Instantiate(theEnemy, new Vector3(xPos, 1, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+            enemyCount += 1;
+        }
+    }
+
+}
