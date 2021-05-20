fe4ba43ec16a69876fe58baff14fd54fea95e761
Timo Zandbergen <zandbergentimo@gmail.com>
2021-05-20 12:11:27 +0200

    1e deel inv

---
+++ Broken Space/Assets/Scriptable Objects/Items/Scripts/ItemObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ItemType
+{
+    Default,
+    Food,
+    Equipment
+}
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public ItemType type;
+    [TextArea(15,20)]
+    public string description;
+
+}
