24528c7d4f7ebd0963c63958ae11eba250f4574b
Timo Zandbergen <zandbergentimo@gmail.com>
2021-06-23 14:54:29 +0200

    enemy 3 times

---
+++ Broken Space/Assets/Scripts/GenerateEnemies3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies3 : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 5)
+        {
+            xPos = Random.Range(29, 40);
+            zPos = Random.Range(13, 20);
+            Instantiate(theEnemy, new Vector3(xPos, 0.4f, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(10f);
+            enemyCount += 1;
+        }
+    }
+
+}
