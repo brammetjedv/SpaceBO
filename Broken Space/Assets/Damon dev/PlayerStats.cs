0416fcc2a657e95d7833afe54c986a2665238fc6
Damon Jellema <damon.jellema2004@gmail.com>
2021-06-23 14:55:37 +0200

    timothy

---
+++ Broken Space/Assets/Damon dev/PlayerStats.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    PlayerUI playerUI;
+    void Start()
+    {
+        playerUI = GetComponent<PlayerUI> ();
+
+
+
+        maxHealth = 100;
+        currHealth = maxHealth;
+
+        SetStats();
+    }
+
+    public override void Die()
+    {
+        Debug.Log("You ded bro");
+    }
+
+    void SetStats()
+    {
+        playerUI.healthAmount.text = currHealth.ToString ();
+    }
+
+    public override void CheckHealth()
+    {
+        base.CheckHealth();
+        SetStats();
+    }
+
+}
